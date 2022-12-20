ad61c35305e9ea65b09e816ad95a98d9d14c59fd
2RohanShewale <shewalerohan50@gmail.com>
2022-12-20 16:25:43 +0530

    position zero

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakesAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snakes and ladder game");
+            int startposition = 0;
+            Console.WriteLine("You are at position " + 0);
+        }
+        
+    }
+}
