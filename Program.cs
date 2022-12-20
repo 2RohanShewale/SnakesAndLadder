e7e8a07227c38f5af7699876194598dc93b3b7c3
2RohanShewale <shewalerohan50@gmail.com>
2022-12-20 16:36:20 +0530

    Roll die

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@
             Console.WriteLine("Welcome to Snakes and ladder game");
             int startposition = 0;
             Console.WriteLine("You are at position " + 0);
+            Console.WriteLine("press any key to roll");
+            Console.ReadKey();
+            Random num = new Random();
+            int die = num.Next(1,7);
+            Console.WriteLine("YOu have rolled this " + die);
         }
         
     }
