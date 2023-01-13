c6ac45bffdf2787c6e634aca9f52e54f78758497
2RohanShewale <shewalerohan50@gmail.com>
2023-01-13 13:05:50 +0530

    Repeat till 100

--- Program.cs
+++ Program.cs
@@ -1,25 +1,56 @@
-﻿namespace SnakesAndLadder
+﻿using System;
+
+namespace SnakesAndLadder
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snakes and ladder game");
-            int startposition = 0;
-            Console.WriteLine("You are at position " + 0);
-            Console.WriteLine("press any key to roll");
+            Console.WriteLine("*--------Welcome To Snakes and Ladder Game--------*");
+            Console.WriteLine();
+            Console.WriteLine("Press key to start the Game.");
             Console.ReadKey();
-            
-            
-            Random num = new Random();
-            int die = num.Next(1,7);
-            Console.WriteLine("YOu have rolled this " + die);
-            
-            
-            startposition = Options.Operation(die, startposition);
-            if (startposition <= 0) { Console.WriteLine("You went under zero Start again"); }
-            else { Console.WriteLine("You are at position" + startposition); }
+            int Position = 0;
+            bool PlayGame = true;
+            while (PlayGame)
+            {
+                Position = Play(Position);
+                if (Position <= 0) 
+                {
+                    Console.WriteLine("You went under 0: ");
+                    Console.WriteLine();
+                    Position= 0;
+                }
+                else if(Position >= 100)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("*---------------------*");
+            Console.WriteLine("You Won");
+            Console.WriteLine("*---------------------*");
+
         }
-        
+
+        public static int Play(int Position)
+        {
+            Console.WriteLine();
+            Console.WriteLine("You are at position: " + Position);
+            Console.WriteLine("Press a key to Roll.");
+            Console.ReadKey();
+            int Roll = RollDice();
+            Console.WriteLine("You have Rolled: " + Roll);
+            Console.WriteLine();
+            Position = Options.Operation(Roll, Position);
+            return Position;
+        }
+        public static int RollDice()
+        {
+            Random random= new Random();
+            int num = random.Next(1,7);
+            return num;
+        }
     }
+
+    
 }
